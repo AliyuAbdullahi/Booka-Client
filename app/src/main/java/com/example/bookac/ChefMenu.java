343b529d535fc6d036033582bd990b371c17cd1b
Aliyu Abdullahi <aliyu.abdullahi@andela.com>
2015-10-17 22:34:01 +0100

    add chef detail view

--- app/src/main/java/com/example/bookac/ChefMenu.java
+++ app/src/main/java/com/example/bookac/ChefMenu.java
@@ -1,10 +1,14 @@
 package com.example.bookac;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ChefMenu extends AppCompatActivity {
@@ -15,15 +19,24 @@ public class ChefMenu extends AppCompatActivity {
     setContentView (R.layout.activity_chef_menu);
     Toolbar toolbar = (Toolbar) findViewById (R.id.toolbar);
     setSupportActionBar (toolbar);
+    getSupportActionBar ().setTitle ("");
+    getSupportActionBar ().setDisplayHomeAsUpEnabled (true);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById (R.id.fab);
-    fab.setOnClickListener (new View.OnClickListener () {
-      @Override
-      public void onClick (View view) {
-        Snackbar.make (view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction ("Action", null).show ();
-      }
-    });
+    final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+    upArrow.setColorFilter (getResources ().getColor (android.R.color.white), PorterDuff.Mode.SRC_ATOP);
+    getSupportActionBar().setHomeAsUpIndicator (upArrow);
+
+
+  }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater ().inflate (R.menu.chef_menu, menu);
+    return true;
   }
 
+  @Override
+  public boolean onOptionsItemSelected (MenuItem item) {
+    return super.onOptionsItemSelected (item);
+  }
 }
